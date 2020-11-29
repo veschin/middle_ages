33ec7df73832d14681c688ed83de3be33d3e91e7
Oleg Veschin <unclemadolu2@gmail.com>
2020-11-29 17:14:43 +0700

    data-fill: added fill-kings func

--- src/data_fill.clj
+++ src/data_fill.clj
@@ -1,1 +1,21 @@
-(ns data-fill)
+(ns data-fill
+  (:require [clojure.java.jdbc :as jdbc]
+            [db :refer [db-spec]]
+            [honeysql.core :as sql]
+            [gen-algo :refer [generate-king]]))
+
+(defn fill-kings []
+  (jdbc/insert-multi! db-spec :kings (map generate-king (range 1000))))
+
+(sort (let [kings (jdbc/query
+                   db-spec
+                   (sql/format {:select [:*]
+                                :from [:kings]}))]
+        (group-by :age kings)))
+;  (jdbc/insert! db-spec :kings {:name "123"
+;                               :sex 1
+;                               :age 30
+;                               :birth_date 930
+;                               :death_date 960
+;                               :heir_id 2
+;                               :years_at_the_throne 20})
