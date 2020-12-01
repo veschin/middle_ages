beec3ddfab48e04416dfb0bd0a00947cf3b5d5bb
Oleg Veschin <unclemadolu2@gmail.com>
2020-12-01 22:58:54 +0700

    data-fill: added fill funcs

--- src/data_fill.clj
+++ src/data_fill.clj
@@ -1,21 +1,43 @@
 (ns data-fill
   (:require [clojure.java.jdbc :as jdbc]
-            [db :refer [db-spec]]
+            [db :refer [db-spec create-database]]
             [honeysql.core :as sql]
-            [gen-algo :refer [generate-king]]))
+
+            [gen-algo :refer [drop-states]]))
 
 (defn fill-kings []
-  (jdbc/insert-multi! db-spec :kings (map generate-king (range 1000))))
+  (drop-states)
+  (jdbc/insert-multi! db-spec :kings (map gen-algo/generate-king (range 1000))))
 
-(sort (let [kings (jdbc/query
-                   db-spec
-                   (sql/format {:select [:*]
-                                :from [:kings]}))]
-        (group-by :age kings)))
-;  (jdbc/insert! db-spec :kings {:name "123"
-;                               :sex 1
-;                               :age 30
-;                               :birth_date 930
-;                               :death_date 960
-;                               :heir_id 2
-;                               :years_at_the_throne 20})
+(defn fill-nobles []
+  (drop-states)
+  (jdbc/insert-multi! db-spec :barons (map gen-algo/generate-noble (range 1000)))
+  (drop-states)
+  (jdbc/insert-multi! db-spec :dukes (map gen-algo/generate-noble (range 1000)))
+  (drop-states)
+  (jdbc/insert-multi! db-spec :earls (map gen-algo/generate-noble (range 1000))))
+
+(defn fill-knights []
+  (drop-states)
+  (jdbc/insert-multi! db-spec :knights (map gen-algo/generate-knight (range 1000))))
+
+(defn fill-peasants []
+  (drop-states)
+  (jdbc/insert-multi! db-spec :peasants (map gen-algo/generate-peasant (range 3000))))
+
+(comment
+  (do
+    (create-database)
+    (fill-kings)
+    (fill-nobles)
+    (fill-knights)
+    (fill-peasants))
+  ;
+  (letfn [(get-table [table]
+            (jdbc/query
+             db-spec
+             (sql/format {:select [:*]
+                          :from [table]})))]
+    (take 100 (get-table :peasants)))
+  ;
+  )
